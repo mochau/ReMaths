af497062c8cdbc9d7a073595f5228bbca33b05d3
mochau <michaelchau97@gmail.com>
2019-04-15 15:32:47 +1000

    First Test Commit

--- app/src/main/java/com/example/t_micha/remaths/MainActivity.java
+++ app/src/main/java/com/example/t_micha/remaths/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    //test branch
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/t_micha/remaths/MathAPI.java
+++ app/src/main/java/com/example/t_micha/remaths/MathAPI.java
@@ -28,4 +28,3 @@ public class MathAPI {
         return dummyMathsResponse;
     }
 }
-
